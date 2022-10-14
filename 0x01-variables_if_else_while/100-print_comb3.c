480ae59432d5797535e7ef302d179ced35f7aa7a
EsChris <themainchris@gmail.com>
2022-10-14 16:20:03 -0700

    Task 10, new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- *main - Prints the numbers from 00 to 99, numbers separated by
- *a comma followed by a space, in ascending order.
+ *main - Printsall possible combinations of two different digits,
+ *in ascending order, separated by a comma followed by a space..
  *Return: Always 0.
  **/
 int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
