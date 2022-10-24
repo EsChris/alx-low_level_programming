a90d095395beb1b1d1b66380633ae8f4d998feec
EsChris <themainchris@gmail.com>
2022-10-24 16:49:11 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,13 @@
 
 int _strlen(char *s)
 {
-	int p = 0;
-	/incremeant up to when the last character is NULL,\0/
-		while (*(s + p) != 0)
-		{
-			p++;
-		}
-	return (p);
-}
+	int i = 1, sum = 0;
+	char pl = s[0];
 
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
+}
