63dc3b6a87b0dec79a4d5871cdd6855105b0a567
EsChris <themainchris@gmail.com>
2022-11-07 14:12:31 -0800

    Task 5 , final commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
 	{
 	cents = atoi(argv[1]);
 	while (cents > 0)
+	{
 		if (cents >= 25)
 			cents -= 25;
 		else if (cents >= 10)
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
 		else if (cents >= 2)
 			cents -= 2;
 		else if (cents >= 1)
-		cents -= 1;
+			cents -= 1;
 		coins_number += 1;
 	}
 	}
